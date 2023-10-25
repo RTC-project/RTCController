e3b222deed8b5318e28e14ff5cef578e42d85a16
DMITRY ZAKHAROV <50522024+dem100288@users.noreply.github.com>
2023-10-25 16:13:06 +0300

    add two wheel

--- line_arduino/line_arduino.ino
+++ line_arduino/line_arduino.ino
@@ -1,4 +1,4 @@
-#define BUF_SIZE 10
+#define BUF_SIZE 14
 #define MEAN_POWER 250
 
 int n_buf = 0;
@@ -9,11 +9,13 @@ union ArrayToInteger {
   int integer;
 }ati;
 
-int m1 = 0, m2 = 0, m3 = 0, m4 = 0;
+int m1 = 0, m2 = 0, m3 = 0, m4 = 0, m5 = 0, m6 = 0;
 
 void setup() {
   Serial.begin(57600);
   Serial3.begin(57600);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
@@ -22,6 +24,8 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT);
 
   for(int i = 0; i < BUF_SIZE; i++){
     rx_buf[i] = 0;
@@ -35,7 +39,7 @@ void loop() {
       rx_buf[n_buf] = b;
       n_buf++;
     }
-    else if (n_buf == 8){
+    else if (n_buf == 12){
       ati.array[0] = rx_buf[0];
       ati.array[1] = rx_buf[1];
       m1 = ati.integer;
@@ -48,6 +52,12 @@ void loop() {
       ati.array[0] = rx_buf[6];
       ati.array[1] = rx_buf[7];
       m4 = ati.integer;
+      ati.array[0] = rx_buf[8];
+      ati.array[1] = rx_buf[9];
+      m5 = ati.integer;
+      ati.array[0] = rx_buf[10];
+      ati.array[1] = rx_buf[11];
+      m6 = ati.integer;
       for(int i = 0; i < BUF_SIZE; i++){
         rx_buf[i] = 0;
       }
@@ -57,18 +67,26 @@ void loop() {
       m2 -= MEAN_POWER;
       m3 -= MEAN_POWER;
       m4 -= MEAN_POWER;
+      m5 -= MEAN_POWER;
+      m6 -= MEAN_POWER;
       int dm1 = m1 < 0 ? 0 : 1;
       int dm2 = m2 < 0 ? 1 : 0;
       int dm3 = m3 < 0 ? 1 : 0;
-      int dm4 = m4 < 0 ? 0 : 1;
-      analogWrite(6, abs(m1));
-      analogWrite(7, abs(m2));
-      analogWrite(8, abs(m3));
-      analogWrite(9, abs(m4));
+      int dm4 = m4 < 0 ? 1 : 0;
+      int dm5 = m5 < 0 ? 0 : 1;
+      int dm6 = m6 < 0 ? 0 : 1;
+      analogWrite(4, abs(m1));
+      analogWrite(5, abs(m2));
+      analogWrite(6, abs(m3));
+      analogWrite(7, abs(m4));
+      analogWrite(8, abs(m5));
+      analogWrite(9, abs(m6));
       digitalWrite(10, dm1);
       digitalWrite(11, dm2);
       digitalWrite(12, dm3);
       digitalWrite(13, dm4);
+      digitalWrite(22, dm5);
+      digitalWrite(33, dm6);
       //Serial.print(m1);
       //Serial.print(" ");
       //Serial.print(m2);
